c6a2da35fc000fe30dac536e4bc8ec4f11ab3c34
Asterki <asterki.dev@proton.me>
2026-02-12 19:15:26 -0600

    Primera commit

---
+++ EjercicioGit/Program.cs
@@ -0,0 +1,15 @@
+﻿// 20251900154 Fernando Rivera
+
+using System;
+
+namespace EjercicioGit
+{
+    internal class Proyecto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Inicializador proyecto
+        }
+    }
+}
